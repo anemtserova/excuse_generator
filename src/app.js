e8f2aef8fd4fcefe8463125359356da92c8308ca
Antoniya <anemtserova@gmail.com>
2021-04-14 13:19:51 +0000

    excuse generator is in working condition

--- src/app.js
+++ src/app.js
@@ -14,19 +14,36 @@ window.onload = function() {
     "The Big Yellow bird",
     "Harry Potter"
   ];
-  let action = ["ate", "peed on", "crushed", "broke", "drank", "stole"];
+  let action = [
+    "ate",
+    "peed on",
+    "crushed",
+    "broke",
+    "drank",
+    "stole",
+    "replaced"
+  ];
   let what = [
     "my homework",
-    "the keys",
+    "the keys to my house",
     "the car",
     "my grandma's glasses",
     "my time machine"
   ];
   let when = [
     "before the class",
-    "right on time",
-    "when I was not looking",
+    "in the hallway",
+    "when I wasn't looking",
     "during my third lunch",
-    'while I was singiing "Jingle Bells"'
+    'while I was singing "Jingle Bells"'
   ];
+
+  let excuse = document.getElementById("excuse");
+
+  excuse.innerHTML = `${who[Math.floor(Math.random() * (who.length - 1))]}
+  ${action[Math.floor(Math.random() * (action.length - 1))]} ${
+    what[Math.floor(Math.random() * (what.length - 1))]
+  } ${when[Math.floor(Math.random() * (when.length - 1))]}`;
+  document.getElementById("test").innerHTML = Math.floor(Math.random() * 6);
+  //console.log(w
 };
